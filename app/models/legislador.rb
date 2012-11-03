6f4d247735fcf08a2b948ba4b74a06163a826ba9
Nicolás Cerrini <ceneon@gmail.com>
2012-11-03 00:31:10 -0300

    Foto para los legisladores

--- app/models/legislador.rb
+++ app/models/legislador.rb
@@ -7,4 +7,24 @@ class Legislador < ActiveRecord::Base
 
 	#has_and_belongs_to_many :comisions
 
+	def url_foto_mediana
+		if self.es_diputado? and self.key.present?
+			"http://www1.hcdn.gov.ar/fotos/" + self.key + "_mediana.jpg"
+		elsif self.es_senador?
+			""
+		else
+			""
+		end
+	end
+
+	def url_foto_grande
+		if self.es_diputado? and self.key.present?
+			"http://www1.hcdn.gov.ar/fotos/" + self.key + ".jpg"
+		elsif self.es_senador?
+			""
+		else
+			""
+		end
+	end
+
 end
